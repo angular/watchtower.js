db081e4b18e1ba3820caa389f3016a040c017a10
Caitlin Potter <caitpotter88@gmail.com>
2014-03-02 17:49:48 -0500

    chore(karma): add IE* custom browsers for CI
    These will be removed if they are too expensive (or if anyone tells me to!), but ideally this library
    should support IE9.

--- karma-shared.conf.js
+++ karma-shared.conf.js
@@ -44,7 +44,25 @@ module.exports = function(config) {
         browserName: 'safari',
         platform: 'OS X 10.9',
         version: '7'
-      }
+      },
+      'SL_IE_9': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 2008',
+        version: '9'
+      },
+      'SL_IE_10': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 2012',
+        version: '10'
+      },
+      'SL_IE_11': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 8.1',
+        version: '11'
+      },
     },
 
 
