c6e3b9c9c89681e836c121ee1d5bb27f14324379
Tobias Bosch <tbosch1009@gmail.com>
2014-03-28 17:16:40 -0700

    feat(api): create index.js with public api for watchtower

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// public API for node.js and browser
+/* jshint ignore:start */
+// TODO: JsHint right now fails for this code!
+export * from './ast';
+export * from './change_detection';
+export * from './dirty_checking';
+export * from './watch_group';
+/* jshint ignore:end */
